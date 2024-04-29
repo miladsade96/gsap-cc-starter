2dff9dd1b1f6d19ebc3d7d46ea49f8f2122c82a5
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-29 16:55:57 +0330

    Implementing gsap.fromTo() method

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,5 +1,22 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+
 const GsapFromTo = () => {
-  // TODO: Implement the gsap.fromTo() method
+  useGSAP(() => {
+    gsap.fromTo(
+      "#red-box",
+      { x: 0, rotation: 0, borderRadius: "10%" },
+      {
+        x: 300,
+        repeat: -1,
+        yoyo: true,
+        rotation: 360,
+        borderRadius: "100%",
+        duration: 4,
+        ease: "sine.inOut",
+      },
+    );
+  }, []);
 
   return (
     <main>
